28d79e2887681bbc1333ba90bb18b7461491bab7
AneeshaRama <aneesharama@gmail.com>
2022-05-11 03:19:47 +0530

    Added _document file

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => {
   return (
     <>
       <nav className="relative">
-        <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide overflow-y-hidden">
+        <div className="flex px-10 sm:px-20 text-lg  whitespace-nowrap space-x-10 sm:space-x-16 overflow-x-scroll scrollbar-hide overflow-y-hidden">
           {Object.entries(requests).map(([key, { title, url }]) => (
             <h2
               onClick={() => Router.push(`/?genre=${key}`)}
               key={key}
-              className="last:pr-24 cursor-pointer transition transform duration-100 hover:scale-125 hover:text-green-400 active:text-red-400"
+              className="font-semibold last:pr-24 cursor-pointer transition transform duration-100 hover:scale-125 hover:text-green-400 active:text-red-400"
             >
               {title}
             </h2>
--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Stream TV and Movies with HULU</title>
-        <link
-          rel="shortcut icon"
-          href="https://assetshuluimcom-a.akamaihd.net/h3o/icons/favicon.ico.png"
-          type="image/x-icon"
-        />
       </Head>
       <Component {...pageProps} />
     </>
